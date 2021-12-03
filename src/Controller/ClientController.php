2955b6d388c100dd9231aa619e9949bf4a59626d
Pierre445 <pierrecastro038@gmail.com>
2021-12-03 15:23:23 +0400

    ajout showall client

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,25 +3,32 @@
 namespace App\Controller;
 
 use App\Entity\Client;
+use App\Entity\Modele;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/client", name="client/")
+ */
+
 class ClientController extends AbstractController
 {
     /**
-     * @Route("/client", name="client")
+     * @Route("/showAll", name="showAll")
      */
     public function showAll(ManagerRegistry $doctrine): Response
     {
         $em = $doctrine->getManager();
-        $repo = $em->getRepository(Client::class);
 
-        $listeClient = $repo->findAll();
+        $repo = $em->getRepository(Client::class) ;
+
+        $listeAll = $repo->findAll();
 
         return $this->render('client/showAllClient.html.twig', [
-            'listeClient' => $listeClient,
+            'listeAll' => $listeAll,
+            'title' => 'Platefome de vente automobile - Liste des clients'
         ]);
     }
 }
