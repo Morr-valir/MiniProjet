aba9e4be1dbcf024212b26011dd4c3c678818309
Morr-valir <77137944+Morr-valir@users.noreply.github.com>
2022-01-19 11:23:00 +0400

    migration update

--- migrations/Version20220119072152.php
+++ migrations/Version20220119072152.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220119053947 extends AbstractMigration
+final class Version20220119072152 extends AbstractMigration
 {
     public function getDescription(): string
     {
